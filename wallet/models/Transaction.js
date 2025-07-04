354c243587f11f9a6bc9c25153af62f9ed040b1c
fixercoin <khanbabusargodhaa@gmail.com>
2025-07-04 15:26:36 +0500

    Update Transaction.js

--- wallet/models/Transaction.js
+++ wallet/models/Transaction.js
@@ -1,1 +1,12 @@
+ const mongoose = require('mongoose');
 
+const transactionSchema = new mongoose.Schema({
+  wallet: { type: mongoose.Schema.Types.ObjectId, ref: 'Wallet' },
+  type: String,
+  amount: Number,
+  timestamp: Date
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
