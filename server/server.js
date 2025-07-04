770f5223cf8f10f1bd83c9d0b4caf233cf0d467f
fixercoin <khanbabusargodhaa@gmail.com>
2025-07-04 13:08:50 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
- 
+ const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost/fixorium', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.json());
+app.use(express.static('client'));
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
