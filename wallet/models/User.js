bc6e350ded43bb48e1302f3ce31438a9cd4343a4
fixercoin <khanbabusargodhaa@gmail.com>
2025-07-04 15:23:46 +0500

    Update User.js

--- wallet/models/User.js
+++ wallet/models/User.js
@@ -1,1 +1,12 @@
+ const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  wallet: { type: mongoose.Schema.Types.ObjectId, ref: 'Wallet' }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
