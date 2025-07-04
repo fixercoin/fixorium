e333ba80c2d075ab9268b75b431defe0f7569d75
fixercoin <khanbabusargodhaa@gmail.com>
2025-07-04 13:15:34 +0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+    fetchBlocks();
+
+    function fetchBlocks() {
+        $.get('/api/blocks', function(data) {
+            data.forEach(block => {
+                $('#block-container').append(`<div>Block #${block.index}: ${block.data}</div>`);
+            });
+        });
+    }
+});
 
