3a703b0d25736ac01c3bc3074bf751edb2ec5d79
fixercoin <khanbabusargodhaa@gmail.com>
2025-07-04 15:29:45 +0500

    Update wallet.js

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -1,51 +1,43 @@
-let balance = 0;
-let transactionHistory = [];
+ const User = require('./models/User');
+const Wallet = require('./models/Wallet');
+const Transaction = require('./models/Transaction');
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 
-$(document).ready(function() {
-    $('#depositBtn').click(function() {
-        const amount = parseFloat($('#depositAmount').val());
-        if (amount > 0) {
-            balance += amount;
-            transactionHistory.push(`Deposited: ${amount} FIXER`);
-            updateBalance();
-            $('#depositAmount').val('');
-        }
-    });
-
-    $('#withdrawBtn').click(function() {
-        const amount = parseFloat($('#withdrawAmount').val());
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
-            transactionHistory.push(`Withdrew: ${amount} FIXER`);
-            updateBalance();
-            $('#withdrawAmount').val('');
-        }
-    });
-
-    $('#swapBtn').click(function() {
-        const amount = parseFloat($('#swapAmount').val());
-        if (amount > 0 && amount <= balance) {
-            balance -= amount; // Assuming a 1:1 swap for simplicity
-            transactionHistory.push(`Swapped: ${amount} FIXER`);
-            updateBalance();
-            $('#swapAmount').val('');
-        }
-    });
-
-    $('#addTokenBtn').click(function() {
-        const amount = parseFloat($('#addTokenAmount').val());
-        if (amount > 0) {
-            transactionHistory.push(`Added Token: ${amount}`);
-            $('#addTokenAmount').val('');
-        }
-    });
-
-    $('#historyBtn').click(function() {
-        $('#history').html(transactionHistory.join('<br>'));
-    });
-
-    function updateBalance() {
-        $('#balanceAmount').text(balance);
+class WalletScript {
+  async createUser(username, password, email) {
+    try {
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        throw new Error('Username already exists');
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ username, password: hashedPassword, email });
+      await user.save();
+      return user;
+    } catch (error) {
+      console.error(error);
     }
-});
+  }
 
+  async createWallet(userId) {
+    try {
+      const existingWallet = await Wallet.findOne({ user: userId });
+      if (existingWallet) {
+        throw new Error('Wallet already exists for this user');
+      }
+      const wallet = new Wallet({ user: userId, balance: 0 });
+      await wallet.save();
+      const user = await User.findById(userId);
+      user.wallet = wallet._id;
+      await user.save();
+      return wallet;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Other methods...
+}
+
+module.exports = WalletScript;
